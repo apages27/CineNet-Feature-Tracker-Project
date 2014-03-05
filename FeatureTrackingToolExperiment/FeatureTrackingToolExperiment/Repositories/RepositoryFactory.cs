9cd60d317ea25bacaecfbc7e9959f1fc85dc643b
apages27 <apages27@gmail.com>
2014-03-05 11:21:50 -0500

    Updated RepositoryFactory.cs file.

---
+++ FeatureTrackingToolExperiment/FeatureTrackingToolExperiment/Repositories/RepositoryFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FeatureTrackingToolExperiment.Interfaces;
+
+namespace FeatureTrackingToolExperiment.Repositories
+{
+    public class RepositoryFactory
+    {
+        public static IFeatureRepository GetFeatureRepository(string mode)
+        {
+            if (mode == "T") return new TestRepository(); else return new FeatureRepository();
+        }
+    }
+}
